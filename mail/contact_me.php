711c5215724b80f1720bc5ed9a5060c9fe164cf6
ValentyKo1 <32367925+ValentyKo1@users.noreply.github.com>
2018-05-18 03:01:22 +0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,24 @@
-<?php    
-    $message = $_POST['message'];
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $from = 'From: valentyko1.github.io'; 
-    $to = 'vetall17081996@gmail.com'; 
-    $subject = 'Hello';
-    $human = $_POST['human'];
-			
-    $body = "From: $name\n E-Mail: $email\n  Message:\n $message";
-				
-    if ($_POST['submit'] && $human == '4') {				 
-        if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent! I will get back to you in 24 hours! Go <a href="htvalentyko1.github.io">back.</a></p>';
-		
-	} else { 
-	    echo '<p>Something went wrong, go back and try again! Go <a href="http://valentyko1.github.io/hire.html">back.</a></p>'; 
-	} 
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly! Go <a href="http://valentyko1.github.io/hire.html">back.</a></p>';
-    }
-
-
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'vetall17081996@gmail.com'; send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: vetall17081996@gmail.com\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
 ?>
